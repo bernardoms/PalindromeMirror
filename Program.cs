62f2238b45abdb6bf183931a3b9c2dab0c88336f
bernardoms <bernardo@csu.fullerton.edu>
2017-07-02 17:14:43 -0300

    Adicionado a versão C# do codigo

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.CurrentDirectory + @"\in.txt";
+            Console.WriteLine("EXERICIO DA MODAL ---------- PALINDROME E MIRROR");
+            Util util = new Util();
+            var lines = System.IO.File.ReadAllLines(path);
+            ArrayList palavras = new ArrayList();
+            // Display the file contents by using a foreach loop.
+            foreach (string line in lines)
+            {
+                palavras.Add(line);
+                // Use a tab to indent each line of the file.    
+            }
+            for(int i = 0; i<palavras.Count;i++)
+            {
+                //Console.WriteLine("\t" + palavras[i]);
+                util.printaMirrorPalindrome(palavras[i].ToString());
+            }
+            Console.WriteLine("\nPressione qualquer tecla para Sair.");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modal
+{
+    class Util
+    {
+        public String invertePalavra(String palavra)
+        {
+            String novaPalavra = "";
+            char[] aux;
+            aux = palavra.ToCharArray(0, palavra.Length);
+            for (int i = palavra.Length - 1; i >= 0; i--)
+            {
+                novaPalavra += aux[i].ToString();
+            }
+            //Console.Write("\n" + novaPalavra);
+            return novaPalavra;
+        }
+
+        public bool verificaMirror(String palavra)
+        {
+            //Transforma uma string em um array de caracteres para consultar suas posições
+            char[] charArray = palavra.ToUpper().ToCharArray();
+            String aux = "";
+            String auxInvertido;
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (charArray[i] == 'E')
+                {
+
+                    charArray[i] = '3';
+                }
+                else if (charArray[i] == '3')
+                {
+
+                    charArray[i] = 'E';
+                }
+                if (charArray[i] == 'S')
+                {
+
+                    charArray[i] = '2';
+                }
+                else if (charArray[i] == '2')
+                {
+
+                    charArray[i] = 'S';
+                }
+                if (charArray[i] == 'J')
+                {
+
+                    charArray[i] = 'L';
+                }
+                else if (charArray[i] == 'L')
+                {
+
+                    charArray[i] = 'J';
+                }
+                if (charArray[i] == 'Z')
+                {
+
+                    charArray[i] = '5';
+                }
+                else if (charArray[i] == '5')
+                {
+
+                    charArray[i] = 'Z';
+                }
+                aux += charArray[i];
+            }
+            //Se ler a palavra mirror de de tras para frente for igual a palavra original, então ela é uma String mirror
+            auxInvertido = this.invertePalavra(aux);
+            if (auxInvertido.ToUpper().Equals(palavra.ToUpper()))
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool verificaPalindrome(String palavra, String palavraInvertida)
+        {
+            if (palavra.ToUpper().Equals(palavraInvertida.ToUpper()))
+            {
+                return true;
+            }
+            return false;
+        }
+        public void printaMirrorPalindrome(String palavra)
+        {
+            bool palindrome;
+            bool mirror;
+            String novaPalavra;
+            novaPalavra = this.invertePalavra(palavra);
+            palindrome = verificaPalindrome(palavra, novaPalavra);
+            mirror = verificaMirror(palavra);
+            if (palindrome && !mirror)
+            {
+                Console.WriteLine(palavra + "-- is a regular palindrome.");
+            }
+            if (mirror && !palindrome)
+            {
+                Console.WriteLine(palavra + "-- is a mirrored string.");
+            }
+            if (palindrome && mirror)
+            {
+                Console.WriteLine(palavra + " -- is a mirrored palindrome.");
+            }
+            if (!palindrome && !mirror)
+            {
+                Console.WriteLine(palavra + " -- is not a palindrome.");
+            }
+        }
+    }
+
+}
